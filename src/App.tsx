796972aa711c91a6bb5ca37d3b6b4ceef8873fd4
MatthewWilsonX <djpduz2s4ud@hotmail.com>
2025-05-18 09:43:17 +0800

    basic react app structure
    created main app component and styling
    added typescript config and public html

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Contract Reader</h1>
+        <p>Analyze Ethereum Smart Contracts</p>
+      </header>
+      <main className="App-main">
+        <div className="container">
+          <p>Enter a contract address to get started</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
